c77e728c2497887b1444cd4bd5514324de961864
derterus <nikitarabcev69@gmail.com>
2024-02-03 13:36:15 +0800

    footer,headre, pages, auth,registr(include.js),profile

--- js/include.js
+++ js/include.js
@@ -1,3 +1,46 @@
 fetch('layouts/header.html')
-.then(response=>response.text())
-    .then(data=>{document.querySelector('#header-container').innerHTML=data;});
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('#header-container').innerHTML = data;
+        var profile = document.querySelectorAll('.profile');
+        var authLinks = document.querySelectorAll('.auth');
+        if (localStorage.getItem('isLoggedIn') === 'true') {
+            authLinks.forEach(function(link) {
+                link.style.display = 'none';
+            });
+            profile.forEach(function(link) {
+                link.style.display = 'block';
+            });
+        } else {
+            authLinks.forEach(function(link) {
+                link.style.display = 'block';
+            });
+            profile.forEach(function(link) {
+                link.style.display = 'none';
+            });
+        }
+    });
+
+fetch('layouts/footer.html')
+.then(response => response.text())
+.then(data => {
+    document.querySelector('#footer-container').innerHTML = data;
+    var profile = document.querySelectorAll('.profile');
+    var authLinks = document.querySelectorAll('.auth');
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+        authLinks.forEach(function(link) {
+            link.style.display = 'none';
+        });
+        profile.forEach(function(link) {
+            link.style.display = 'block';
+        });
+    } else {
+        authLinks.forEach(function(link) {
+            link.style.display = 'block';
+        });
+        profile.forEach(function(link) {
+            link.style.display = 'none';
+        });
+    }
+});
+    
