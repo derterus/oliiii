a56e04e83578462bb4c90a7cf042ea1e96666905
derterus <nikitarabcev69@gmail.com>
2024-02-08 13:35:53 +0800

    ghghg

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
+ function ActiveCloseclick() {
+ let x = document.getElementById('hidden-element-active');
+      if (x.style.display == "none"){
+   x.style.display = "block";
+   } else {
+ x.style.display = "none"}
+    };
 
-    
+    function ModerateCloseclick() {
+      let x = document.getElementById('hidden-element-moderate');
+           if (x.style.display == "none"){
+        x.style.display = "block";
+        } else {
+      x.style.display = "none"}
+         };
+
+         function FoundCloseclick() {
+          let x = document.getElementById('hidden-element-found');
+               if (x.style.display == "none"){
+            x.style.display = "block";
+            } else {
+          x.style.display = "none"}
+             };
+
+             function ArchiveCloseclick() {
+              let x = document.getElementById('hidden-element-archive');
+                   if (x.style.display == "none"){
+                x.style.display = "block";
+                } else {
+              x.style.display = "none"}
+                 };
